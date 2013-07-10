7a7f010ed8c6248ec5c04f73e1f75e06c7cc551b
Mikko Värri <vmj@linuxbox.fi>
2013-07-10 18:02:59 +0300

    Version 0.4

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* See info libc -> Argp Global Variables */
-const char * argp_program_version = "0.3";
+const char * argp_program_version = "0.4";
 const char * argp_program_bug_address = "vmj@linuxbox.fi";
